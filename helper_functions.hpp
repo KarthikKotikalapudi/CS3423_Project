b68ea0a87cf2552c870dba9344d43d8b65c7f2a0
Kotikalapudi Karthik <cs21btech11030@iith.ac.in>
2023-11-05 16:15:50 +0530

    Modified search_symtab func

--- helper_functions.hpp
+++ helper_functions.hpp
@@ -4,4 +4,4 @@ int insert_symtab(std::string name,std::string type,std::vector<int>dim,int leve
 int delete_symtab_level(int level);
 // searches the symbol table starting from given level and decrementing one by one, for given name
 // and returns the record if found. main bool indicates if search is done from main funnction or other function
-symtab search_symtab(std::string name,int level,bool main);
+symtab search_symtab(std::string name,int level);
