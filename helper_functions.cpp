7e7cc4945d128a0078d56c7db5f2dfcc2db05bf2
Kotikalapudi Karthik <cs21btech11030@iith.ac.in>
2023-11-06 16:56:44 +0530

    Added class helper functions and some semantics

--- helper_functions.cpp
+++ helper_functions.cpp
@@ -28,14 +28,14 @@ void delete_symtab_level(int level)
        }
 }
 
-symtab search_symtab(std::string name,int level)
+symtab search_symtab(std::string name,int level, bool func)
 {
        int i = level;
        while(i>=0)
        {
             if(!sym_table_list[i][name]) return sym_table_list[i][name];
             i--;
-            if(i == 1)      i--;
+            if(func && i == 1)      i--;
        }
        return NULL;
 }
@@ -53,4 +53,76 @@ functab search_functab(std::string name, std::vector<std::string>args){
               if(i->params == args)   return i;
        }
        return NULL;
+}
+
+bool coersible(string a, string b){
+       if (a==b) return true;
+       if(a =="int" && b=="float" || a=="float" && b=="int" || a=="int" && b=="bool" || a=="bool" && b=="int" || a=="float" && b=="bool" || a=="bool" && b=="float"){
+              return true;
+       }
+       return false;
+}
+
+int insert_classtab(std::string name, std::pair<std::string,std::string>Inherited){
+       classtab c_table = new struct class_table;
+       c_table->name = name;
+       if(Inherited.second != ""){
+              classtab temp = search_classtab(Inherited.second);
+              if(!temp){
+                     return 1;
+              }
+              c_table->inherited = Inherited;
+       }
+       class_table_list[name] = c_table;
+       return 0;
+}
+
+classtab search_classtab(std::string name){
+       auto it = class_table_list.find(name);
+       if(it != class_table_list.end()){
+              return class_table_list[name];
+       }       
+       return NULL;
+}
+
+void insert_classvar(std::string name, std::string type, std::string access, classtab c){
+       std::pair<std::string,std::string> temp;
+       temp.first = type;
+       temp.second = access;
+       c->vars[name] = temp;
+}
+
+std::pair<std::string,std::string> search_classvar(std::string name, std::string class_name){
+       std::pair<std::string,std::string> temp;
+       temp.first = "";
+       temp.second = "";
+       classtab c = search_classtab(class_name);
+       if(!c) return temp;
+       auto it = c->vars.find(name);
+       if(it != c->vars.end()){
+              temp = c->vars[name];
+       }
+       return temp;
+}
+
+void insert_classfunc(std::string name, std::string return_type, std::string access, std::vector<std::string>params, classtab c){
+       std::pair<functab,std::string> temp;
+       temp.second = access;
+       functab function_table = new struct func_table;
+       function_table->name = name;
+       function_table->params = params;
+       function_table->return_type = return_type;
+       temp.first = function_table;
+       c->funcs[name] = temp;
+}
+
+std::pair<functab,std::string> search_classfunc(std::string func_name, std::string class_name){
+       classtab c = search_classtab(class_name);
+       auto it = c->funcs.find(func_name);
+       if(it != c->funcs.end()){
+              return c->funcs[func_name];
+       }
+       pair<functab,std::string> temp;
+       temp.first = NULL;
+       return temp;
 }
