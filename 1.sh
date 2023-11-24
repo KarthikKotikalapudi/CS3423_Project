5e7c629a5df1c387d2905c09425137e96c5d676b
Kotikalapudi Karthik <cs21btech11030@iith.ac.in>
2023-11-24 23:28:53 +0530

    Rectified few errors

--- 1.sh
+++ 1.sh
@@ -2,8 +2,6 @@
 bison -d parse.y
 mv parse.tab.c parse.tab.cpp
 sed -i -e 's/parse.tab.c/parse.tab.cpp/g' parse.tab.cpp
-sed -i -e 's/parse.tab.h/parse.tab.hpp/g' parse.tab.cpp
-mv parse.tab.h parse.tab.hpp
 flex  lex.l
 mv lex.yy.c lex.yy.cpp
 g++ lex.yy.cpp parse.tab.cpp helper_functions.cpp
