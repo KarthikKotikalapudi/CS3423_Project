2cedfdba10af22c7b86659171fe20c219ea03f7b
Kotikalapudi Karthik <cs21btech11030@iith.ac.in>
2023-11-25 14:23:51 +0530

    changed file format and included TCs in gitignore

--- 1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-bison -d parse.y
-mv parse.tab.c parse.tab.cpp
-sed -i -e 's/parse.tab.c/parse.tab.cpp/g' parse.tab.cpp
-flex  lex.l
-mv lex.yy.c lex.yy.cpp
-g++ lex.yy.cpp parse.tab.cpp Helper/helper_functions.cpp -o compiler
-rm lex.yy.cpp parse.tab.cpp parse.tab.h
