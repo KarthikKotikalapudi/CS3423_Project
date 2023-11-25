0874226cf2fd400e13cf86f8a36216b403c6190e
Kotikalapudi Karthik <cs21btech11030@iith.ac.in>
2023-11-25 14:17:44 +0530

    Just changing the file format as submitted.

---
+++ 9_COMPILERS2_FINALPROJECT/codes/Helper/global.hpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef struct symbol_table* symtab;
+typedef struct func_table* functab;
+typedef struct class_table* classtab;
+
+struct symbol_table 
+{
+    std::string name;
+    std::string type;
+    std::vector<int> dim;
+    int level;
+};
+typedef struct symbol_table symbolTable;
+
+struct func_table 
+{
+    std::string name;
+    std::vector<std::string> params;
+    std::string return_type;
+    int override = 0;
+};
+
+struct class_table
+{
+    std::string name;
+    std::unordered_map<std::string,vector<std::string>> vars;
+    std::unordered_map<std::string,vector<pair<functab,std::string>>> funcs;
+    std::pair<std::string,std::string> inherited;
+};
+
+extern int scope ;
+extern std::vector<std::unordered_map<std::string,symtab>> sym_table_list;
+extern std::unordered_map<std::string,vector<functab>> func_table_list;
+extern std::unordered_map<std::string,classtab> class_table_list;
+
---
+++ 9_COMPILERS2_FINALPROJECT/codes/Helper/helper_functions.hpp
@@ -0,0 +1,27 @@
+// inserts the given varianble in the symbol table, provide details of variable
+void insert_symtab(std::string name,std::string type,std::vector<int>dim,int level);
+// deletes an entire scope in the symbol provided the level
+void delete_symtab_level(int level);
+// searches the symbol table starting from given level and decrementing one by one, for given name
+// and returns the record if found. main bool indicates if search is done from main funnction or other function
+symtab search_symtab(std::string name,int level, bool func, bool decl);
+
+void insert_functab(std::string name, std::vector<std::string>params, std::string return_type);
+
+functab search_functab(std::string name, std::vector<std::string>args);
+
+bool coersible(string a, string b);
+
+string dominate(string a, string b);
+
+int insert_classtab(std::string name, std::pair<std::string,std::string>Inherited);
+
+classtab search_classtab(std::string name);
+
+void insert_classvar(std::string name, std::string type, std::string access, classtab c, bool override);
+
+std::vector<std::string> search_classvar(std::string name, std::string class_name);
+
+void insert_classfunc(std::string name, std::string return_type, std::string access, std::vector<std::string>params, classtab c, bool override);
+
+std::pair<functab,std::string> search_classfunc(std::string func_name, std::vector<std::string>params, std::string class_name);
